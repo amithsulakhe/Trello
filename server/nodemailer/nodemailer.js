4fa8d4d2e9b7b94578b5d7e54998c0364653aa52
amithsulakhe <amithsulakhe729@gmail.com>
2024-12-29 12:03:51 +0530

    pass removed

--- server/nodemailer/nodemailer.js
+++ server/nodemailer/nodemailer.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   secure: false, // true for port 465, false for other ports
   auth: {
     user: "amithsulakhe2468@gmail.com",
-    pass: "jdrw etbv owzf moqm", // from google app password
+    pass: process.env.NODEMAILERPASSWORD, // from google app password
   },
 });
 
